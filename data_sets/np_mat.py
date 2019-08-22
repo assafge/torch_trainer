b6cfcc6e198f8553e106346f277fb3ca1ea4b6da
assafge <assafge@zoho.com>
2019-08-22 11:31:42 +0300

    data loader random split

--- data_sets/np_mat.py
+++ data_sets/np_mat.py
@@ -1,19 +1,28 @@
 import numpy as np
-from torch.utils.data import Dataset, DataLoader, Sampler
+from torch.utils.data import Dataset, random_split, DataLoader
+from torch import manual_seed
+
 
 class NumpyMatDataset(Dataset):
-    def __init__(self, file_path, data_lbl='data', labels_lbl='labels'):
+    def __init__(self, file_path, seed: int, train_split: float, data_lbl='data', labels_lbl='labels'):
         with open(file_path) as f:
             mat = np.load(f)
         self.data = mat[data_lbl]
         self.labels = mat[data_lbl]
+        self.seed = seed
+        self.train_split = train_split
 
     def __getitem__(self, item):
-        return self.data[item]
+        return self.data[item], self.labels[item]
 
     def __len__(self):
         return self.data.shape[0]
 
-    def get_data_loaders(self):
-        train_sampler = Sampler(self.data)
-        test_sampler = Sampler(self.data)
+    def get_data_loaders(self, batch_size):
+        train_size = int(self.train_split * len(self))
+        test_size = len(self) - train_size
+        manual_seed(self.seed)
+        train_dataset, test_dataset = random_split(self, [train_size, test_size])
+        train_data_loader = DataLoader(train_dataset, batch_size=batch_size)
+        test_data_loader = DataLoader(test_dataset, batch_size=batch_size)
+        return train_data_loader, test_data_loader
