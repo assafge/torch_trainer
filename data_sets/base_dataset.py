32c9bedcb6413baad361a3209862151a228ae086
assafge <a.genosar@gmail.com>
2020-11-19 11:01:20 +0200

    split stage 1 bug fixes

--- data_sets/base_dataset.py
+++ data_sets/base_dataset.py
@@ -1,16 +1,22 @@
 from torch.utils.data import Dataset
 from abc import abstractmethod
+from dataclasses import dataclass
+from typing import Dict
 
 
 class BaseDataset(Dataset):
-    def __init__(self, root_dir: str, in_channels: int, out_channels: int, train_split: float):
+    def __init__(self, root_dir: str, in_channels: int, out_channels: int, train_split: float,
+                 data_sets, params_type: dataclass):
         self.root_dir = root_dir
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.train_split = train_split
+        self.data_sets: Dict[str, params_type] = {}
+        for set_name, prm in data_sets.items():
+            self.data_sets[set_name] = params_type(**prm)
 
     @abstractmethod
-    def get_data_loaders(self):
+    def get_data_loaders(self, batch_size: int):
         pass
 
     @abstractmethod
